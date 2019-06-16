5ef09e08ef1507bbda1b1604e70b315fadd40c6c
SKarolinav <Skkarolinav@gmail.com>
2019-06-16 20:21:13 +0200

    Updates

---
+++ precache-manifest.bf18ebd6b86a5606efce6d0cd663b45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb59cadb4c4214de8f8059f6114c8ca",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "2f3bb76bf0cd0f982548",
+    "url": "/calculator-app/static/css/main.367afd64.chunk.css"
+  },
+  {
+    "revision": "e3e39b5fed11da87d25d",
+    "url": "/calculator-app/static/js/2.fd765dae.chunk.js"
+  },
+  {
+    "revision": "2f3bb76bf0cd0f982548",
+    "url": "/calculator-app/static/js/main.09c2aaa7.chunk.js"
+  },
+  {
+    "revision": "d18058c2abe8e7c507e3",
+    "url": "/calculator-app/static/js/runtime~main.ee496a9f.js"
+  },
+  {
+    "revision": "4a73fbf749b892340f037616cf8dd6e3",
+    "url": "/calculator-app/static/media/Wallpaper.4a73fbf7.jpg"
+  }
+]);
